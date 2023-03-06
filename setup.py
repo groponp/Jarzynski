a3630833463335c4a46891fd3817ab994923eddc
groponp <groponp@gmail.com>
2023-03-06 14:35:35 -0500

    listo para usar en pip

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+readme = open("./README.md", "r")
+
+setup(
+    name="Jarzynski", 
+    packages=['Jarzynski'],
+    version='1.0.2',
+    description='A computational tool to compute PMF and Kd from non-equilibrium simulation',
+    long_description=readme.read(), 
+    long_description_content_type='text/markdown',
+    author='Ropón-Palacios G.',
+    author_email='groponp@gmail.com',
+    url='https://github.com/groponp/Jarzynski',
+    download_url='https://github.com/groponp/Jarzynski/tarball/1.0.2',
+    keywords=['non-equilibrium md', 'jarzynski', 'PMF', 'Kd'],
+    classifiers=[],
+    license='GPLv3',
+    include_package_data=True
+)
---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+from src.compute_jarzynski import * 
+from src.computeKd import * 
