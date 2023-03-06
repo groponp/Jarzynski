455a2085a9c1136b3b314f79a0569aa859bf4beb
groponp <groponp@gmail.com>
2023-03-06 14:49:25 -0500

    se corrigio

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ readme = open("./README.md", "r")
 
 setup(
     name="jarzynski", 
-    packages=['jarzynski'],
+    packages=['src'],
     version='1.0.2',
     description='A computational tool to compute PMF and Kd from non-equilibrium simulation',
     long_description=readme.read(), 
